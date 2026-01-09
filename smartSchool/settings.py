daa02a0205967ef2fdcbdf52841811f617d2ea6c
Chenda <tobchendamm18@gmail.com>
2026-01-09 19:47:53 +0700

    Allow Render domains in ALLOWED_HOSTS

--- smartSchool/settings.py
+++ smartSchool/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-v88^yqsum^*z&377a$i)szm=6b7$syjctu&_c5xpeo35#t)t1y'
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.onrender.com', 'localhost', '127.0.0.1']
 
 # ADD THESE 3 LINES AT THE END OF settings.py
 LOGIN_URL = 'login'
